db2d89b21f218460dfab35d024332a39a9408838
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-01-22 18:36:08 -0300

    creacion de nuevo componente

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NuevoComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ app-angular/src/app/nuevo-componente/nuevo-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-componente',
+  imports: [],
+  templateUrl: './nuevo-componente.component.html',
+  styleUrl: './nuevo-componente.component.css'
+})
+export class NuevoComponenteComponent {
+  tituloNuevoComponente = "Nuevo Componente";
+}
